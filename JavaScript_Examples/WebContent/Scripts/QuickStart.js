a468d92161474595b6b62a9b7bea01cda45b049e
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-22 10:56:31 +0530

    Mouse Down Event

--- JavaScript_Examples/WebContent/Scripts/QuickStart.js
+++ JavaScript_Examples/WebContent/Scripts/QuickStart.js
@@ -1,3 +1,9 @@
 function greet() {
 	console.log("Hello from External");
 }
+
+function show_coords(event) {
+	x = event.clientX
+	y = event.clientY
+	console.log("X coords: " + x + ", Y coords: " + y)
+}
