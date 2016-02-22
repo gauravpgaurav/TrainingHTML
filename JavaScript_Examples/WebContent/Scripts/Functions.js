7a2767d3c21635d90c6fb675b65b766159bec9c9
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-22 15:43:43 +0530

    JS_objects

--- JavaScript_Examples/WebContent/Scripts/Functions.js
+++ JavaScript_Examples/WebContent/Scripts/Functions.js
@@ -2,11 +2,18 @@ function init() {
 
 	var item = {
 		"itemName" : "TV",
-		"itemImage" : "images/tv.jpg"
+		"itemImage" : "images/tv.png",
+		showItem : function() {
+			var ele = document.getElementById("itemName");
+			var img = document.getElementById("pict");
+			ele.innerHTML = this.itemName;
+			img.src = this.itemImage;
+		}
 	};
 
 	console.log("Init Called");
 	console.log(item.itemName);
+	item.showItem();
 	// console.log(item.prototype);
 }
 
