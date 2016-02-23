661991b40c29fc659e5a967895ab1179c1b4fd45
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-23 09:52:15 +0530

    Function Types

--- JavaScript_Examples/WebContent/Scripts/JS_Objects.js
+++ JavaScript_Examples/WebContent/Scripts/JS_Objects.js
@@ -16,18 +16,21 @@ function init() {
 }
 
 function showAllItems() {
+
 	for ( var item in items) {
 		console.log(items[item].itemName);
 		console.log(items[item].itemImage);
-		// console.log(item.itemImage);
-        var header = document.createElement("h2");
-        var text = document.createTextNode(items[item].itemName);
-         header.appendChild(text);
-        
-        var img = document.createElement("img");
-        var att = document.createAttribute("src");       
-        att.value = items[item].itemImage;                          
-        img.setAttributeNode(att);
+
+		var header = document.createElement("h2");
+		document.getElementsByTagName("h2")[item].innerHTML = items[item].itemName;
+		// var text = document.createTextNode(items[item].itemName);
+		// header.appendChild(text);
+		var img = document.createElement("img");
+		var att = document.createAttribute("id");
+		// att.value = items[item].itemImage;
+
+		// img.setAttributeNode(att);
+		document.getElementsByTagName("img")[item].src = items[item].itemImage;
 	}
 }
 (function() {
---
+++ JavaScript_Examples/WebContent/Scripts/JavaScriptFunctions.js
@@ -0,0 +1,59 @@
+function normalFunction() {
+
+	alert("I am a Normal Function");
+	answer.innerHTML = "I am a Normal Function";
+}
+
+function withArgFunction(name) {
+
+	alert("Welcome to JavaScript Functions " + name);
+	answer.innerHTML = "Welcome to JavaScript Functions " + name;
+}
+
+function returnFunction(name) {
+
+	return "Welcome to JavaScript Functions " + name;
+}
+
+function takeFunctionAsArg(func) {
+
+	var recvd = func;
+	alert(recvd.show());
+
+}
+
+function returningFunction() {
+
+	return {
+		addClass : function() {
+			alert("Add Class Called");
+		},
+		removeClass : function() {
+			alert("Remove Class Called");
+		}
+	};
+}
+
+(function() {
+	init();
+})();
+
+function init() {
+	var btn1 = document.getElementById("btn1");
+	var btn2 = document.getElementById("btn2");
+	var btn3 = document.getElementById("btn3");
+	var btn4 = document.getElementById("btn4");
+	var btn5 = document.getElementById("btn5");
+	var answer = document.getElementById("answer");
+
+	btn1.addEventListener("click", normalFunction);
+	btn2.addEventListener("click", normalFunction);
+	btn3.addEventListener("click", normalFunction);
+	btn4.addEventListener("click", normalFunction);
+	btn5.addEventListener("click", function() {
+		var funcList = returningFunction();
+		funcList.addClass();
+		funcList.removeClass();
+	});
+
+}
