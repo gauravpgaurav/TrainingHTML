7b4a9c5ed2258c20abdfca821bbc76bde04a9a9b
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-23 10:15:09 +0530

    Completed

--- JavaScript_Examples/WebContent/Scripts/JavaScriptFunctions.js
+++ JavaScript_Examples/WebContent/Scripts/JavaScriptFunctions.js
@@ -1,36 +1,32 @@
+// Function 1
 function normalFunction() {
-
-	// alert("I am a Normal Function");
 	answer.innerHTML = "I am a Normal Function";
 }
 
+// Function 2
 function withArgFunction(name) {
-
-	alert("Welcome to JavaScript Functions " + name);
 	answer.innerHTML = "Welcome to JavaScript Functions " + name;
 }
 
+// Function 3
 function returnFunction(name) {
-
 	return "Welcome to JavaScript Functions " + name;
 }
 
+// Function 4
 function takeFunctionAsArg(func) {
-
 	var recvd = func;
 	alert(recvd.show());
-
 }
 
+// Function 5
 function returningFunction() {
 
 	return {
 		addClass : function() {
-			// alert("Add Class Called");
 			answer.innerHTML = "Add Class Called ";
 		},
 		removeClass : function() {
-			// alert("Remove Class Called");
 			answer.innerHTML += ", Remove Class Called";
 		}
 	};
@@ -41,21 +37,41 @@ function returningFunction() {
 })();
 
 function init() {
+
 	var btn1 = document.getElementById("btn1");
 	var btn2 = document.getElementById("btn2");
 	var btn3 = document.getElementById("btn3");
 	var btn4 = document.getElementById("btn4");
 	var btn5 = document.getElementById("btn5");
+	var txt = document.getElementById("txt");
 	var answer = document.getElementById("answer");
 
 	btn1.addEventListener("click", normalFunction);
-	btn2.addEventListener("click", normalFunction);
-	btn3.addEventListener("click", normalFunction);
-	btn4.addEventListener("click", normalFunction);
+
+	btn2.addEventListener("click", function() {
+
+		withArgFunction(txt.value);
+
+	});
+
+	btn3.addEventListener("click", function() {
+
+		answer.innerHTML = returnFunction(txt.value);
+
+	});
+
+	btn4.addEventListener("click", function() {
+
+		takeFunctionAsArg(withArgFunction(txt.value));
+
+	});
+
 	btn5.addEventListener("click", function() {
+
 		var funcList = returningFunction();
 		funcList.addClass();
 		funcList.removeClass();
+
 	});
 
 }
