b7bd100ee273f6f3a4c7954ad2376bd2f2ede274
Gaurav Pant <gauravpgaurav@gmail.com>
2016-03-08 14:42:32 +0530

    delete

--- REST__Examples/src/com/training/resources/SimpleResource.java
+++ REST__Examples/src/com/training/resources/SimpleResource.java
@@ -1,9 +1,11 @@
 package com.training.resources;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -43,5 +45,13 @@ public class SimpleResource {
 		message = emp.toString();
 
 		return "One Employee with " + message + " Added";
+	}
+
+	@DELETE
+	@Path("/delete/{empId}")
+	public String deleteEmployee(@PathParam("id") long empId) {
+
+		
+		return "One Employee with id " + empId + " Deleted";
 	}
 }
