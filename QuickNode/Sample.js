a1385ba112e91dd651274fc98ba6ef1682be90a5
Gaurav Pant <gauravpgaurav@gmail.com>
2016-03-29 16:52:19 +0530

    JS using Java

---
+++ QuickNode/Sample.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+(function() {
+
+	var MyJavaClass = Java.type('com.training.MyClass');
+
+	var result = MyJavaClass.fun1('Gaurav Pant');
+
+	print(result);
+
+})();
---
+++ QuickNode/src/com/training/MyClass.java
@@ -0,0 +1,9 @@
+package com.training;
+
+public class MyClass {
+
+	public static String fun1(String name) {
+		System.out.format("Hi there from Java, %s", name);
+		return "Greetings from Java";
+	}
+}
